b64d6c1ce886d47108c835517802214eb24f4bff
Reshma Suresh <reshmasuresh3009@gmail.com>
2018-08-13 12:41:22 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = IPAddress.Any;
+            IPEndPoint ipEnd = new IPEndPoint(ipaddr, 23000);
+            socket.Bind(ipEnd);
+            socket.Listen(5);
+            socket.Accept();
+           
+           
+
+        }
+    }
+}
